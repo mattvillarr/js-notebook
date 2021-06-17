85a3aa3d48f652aa5a8b447d8624226a898991db
Matt Villarreal <mattvillarr@gmail.com>
2021-06-17 00:54:17 -0500

    monaco code editor init

---
+++ src/components/code-editor.tsx
@@ -0,0 +1,28 @@
+import MonacoEditor from '@monaco-editor/react';
+
+interface CodeEditorProps {
+  initialValue: string;
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue }) => {
+  return (
+    <MonacoEditor
+      value={initialValue}
+      language='javascript'
+      theme='dark'
+      options={{
+        wordWrap: 'on',
+        minimap: { enabled: false },
+        showUnused: false,
+        folding: false,
+        lineNumbersMinChars: 3,
+        fontSize: 16,
+        scrollBeyondLastLine: false,
+        automaticLayout: true,
+      }}
+      height='500px'
+    />
+  );
+};
+
+export default CodeEditor;
