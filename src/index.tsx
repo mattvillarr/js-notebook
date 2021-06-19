c837f640ef31e8b601125bc11474b6123a78169e
Matt Villarreal <mattvillarr@gmail.com>
2021-06-19 18:50:42 -0500

    markdown editor init

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import ReactDOM from 'react-dom';
 import CodeCell from './components/code-cell';
+import TextEditor from './components/text-editor';
 
 import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   );
 };
