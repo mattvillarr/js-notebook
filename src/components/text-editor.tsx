d5992639184126120ee0a2a4409cb344b8eab423
Matt Villarreal <mattvillarr@gmail.com>
2021-06-20 14:00:44 -0500

    text editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -6,6 +6,7 @@ import './text-editor.css';
 const TextEditor: React.FC = () => {
   const ref = useRef<HTMLDivElement | null>(null);
   const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState('# Header');
 
   useEffect(() => {
     const listener = (event: MouseEvent) => {
@@ -28,19 +29,22 @@ const TextEditor: React.FC = () => {
 
   if (editing) {
     return (
-      <div ref={ref}>
-        <MDEditor />
+      <div className='text-editor' ref={ref}>
+        <MDEditor value={value} onChange={v => setValue(v || '')} />
       </div>
     );
   }
 
   return (
     <div
+      className='text-editor card'
       onClick={() => {
         setEditing(true);
       }}
     >
-      <MDEditor.Markdown source={'# Header'} />
+      <div className='card-content'>
+        <MDEditor.Markdown source={value} />
+      </div>
     </div>
   );
 };
