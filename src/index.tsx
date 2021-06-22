4a6d80e9411ef1eac40d914362e4160d5085d334
Matt Villarreal <mattvillarr@gmail.com>
2021-06-21 22:10:35 -0500

    redux setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,6 @@
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './state';
 import CodeCell from './components/code-cell';
 import TextEditor from './components/text-editor';
 
@@ -6,9 +8,11 @@ import 'bulmaswatch/superhero/bulmaswatch.min.css';
 
 const App = () => {
   return (
-    <div>
-      <TextEditor />
-    </div>
+    <Provider store={store}>
+      <div>
+        <CodeCell />
+      </div>
+    </Provider>
   );
 };
 
